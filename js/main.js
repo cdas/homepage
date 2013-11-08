a104910b5caef9c6363b0bde78d382a119810aa0
Christoph Daseking <cdas@habmalnefrage.de>
2013-11-08 18:20:41 +0100

    links get included by angularjs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+function LinksCtrl($scope) {
+    $scope.links = [
+        {name:'Email', type:'email', url:'cdas@habmalnefrage.de'},
+        {name:'Twitter', type:'profile', url:'http://twitter.com/cdas31'},
+        {name:'Impressum', type:'page', url:'http://twitter.com/cdas31'}
+    ];
+}

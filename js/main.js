d4d0ff085148e67204810537cced46d29c39de08
Christoph Daseking <cdas@habmalnefrage.de>
2013-11-08 18:23:49 +0100

    added and corrected links

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function LinksCtrl($scope) {
     $scope.links = [
         {name:'Email', type:'email', url:'cdas@habmalnefrage.de'},
         {name:'Twitter', type:'profile', url:'http://twitter.com/cdas31'},
-        {name:'Impressum', type:'page', url:'http://twitter.com/cdas31'}
+        {name:'GitHub', type:'profile', url:'https://github.com/cdas'},
+        {name:'Impressum', type:'page', url:'http://christoph-daseking.de'}
     ];
 }
